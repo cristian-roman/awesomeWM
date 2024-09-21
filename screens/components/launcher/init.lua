5f9c1eb5ac12188544590f3b33d5049c9ff472c2
cristian-roman <romancristianroman@yahoo.com>
2024-09-21 15:20:47 +0300

    (stable)

--- screens/components/launcher/init.lua
+++ screens/components/launcher/init.lua
@@ -2,6 +2,6 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 local menu = require("screens.components.menu")
 
-launcher = awful.widget.launcher({ image = beautiful.awesome_icon,
+launcher = awful.widget.launcher({  image = beautiful.awesome_icon,
                                      menu = menu })
 return launcher

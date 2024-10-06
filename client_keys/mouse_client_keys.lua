17a0300a7eafb709b4c6b8bc2e9d7869facc9823
cristian-roman <romancristianroman@yahoo.com>
2024-10-06 17:58:01 +0300

    workspace controlls in place

--- client_keys/mouse_client_keys.lua
+++ client_keys/mouse_client_keys.lua
@@ -1,18 +1,20 @@
 local gears = require("gears")
 local awful = require("awful")
 
+local variables = require("variables")
+
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
     end),
-    awful.button({ modkey }, 1, function (c)
+    awful.button({ variables.modkey }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.move(c)
     end),
-    awful.button({ modkey }, 3, function (c)
+    awful.button({ variables.modkey }, 3, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.resize(c)
     end)
 )
 
-return awful
+return clientbuttons
--- init/mouse_workspace_controlls.lua
+++ init/mouse_workspace_controlls.lua
@@ -1,7 +1,25 @@
 local gears = require("gears")
 local awful = require("awful")
+local variables = require("variables")
 
 root.buttons(gears.table.join(
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
+
+awful.util.taglist_buttons = gears.table.join(
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ variables.modkey }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ variables.modkey }, 3, function(t)
+        if client.focus then
+            client.focus:toggle_tag(t)
+        end
+    end),
+    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+)

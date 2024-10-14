eee7f338b0cd03e73c137b02536ee219e3cee42b
cristian-roman <romancristianroman@yahoo.com>
2024-10-14 07:52:13 +0300

    (refactor): update readability in rules.lua

--- init/rules.lua
+++ init/rules.lua
@@ -3,45 +3,54 @@ local beautiful = require("beautiful")
 local keyboard_client_keys = require("keybindings.clients.client_keys")
 local mousebindings = require("mousebindings.clients_bindings")
 
-awful.rules.rules = {
-    { rule = { },
-    properties = { border_width = beautiful.border_width,
-    border_color = beautiful.border_normal,
-    focus = awful.client.focus.filter,
-    raise = true,
-    keys = keyboard_client_keys,
-    buttons = mousebindings,
-    screen = awful.screen.preferred,
-    placement = awful.placement.no_overlap+awful.placement.no_offscreen
-  }
+awful.rules.rules = 
+{
+  { 
+    rule = { },
+    properties = { 
+      border_width = beautiful.border_width,
+      border_color = beautiful.border_normal,
+      focus = awful.client.focus.filter,
+      raise = true,
+      keys = keyboard_client_keys,
+      buttons = mousebindings,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_overlap+awful.placement.no_offscreen
+    }
   },
   
-  { rule_any = {
-    instance = {
-      "DTA",
-      "copyq",
-      "pinentry",
-    },
-    class = {
-      "Arandr",
-      "Blueman-manager",
-      "Gpick",
-      "Kruler",
-      "MessageWin",
-      "Sxiv",
-      "Tor Browser",
-      "Wpa_gui",
-      "veromix",
-      "xtightvncviewer"},
-      
-      name = {
-        "Event Tester",
+  { 
+    rule_any = {
+      instance = {
+        "DTA",
+        "copyq",
+        "pinentry",
       },
-      role = {
-        "AlarmWindow",
-        "ConfigManager",
-        "pop-up",
-      }
-    }, properties = { floating = true }},
+
+      class = {
+        "Arandr",
+        "Blueman-manager",
+        "Gpick",
+        "Kruler",
+        "MessageWin",
+        "Sxiv",
+        "Tor Browser",
+        "Wpa_gui",
+        "veromix",
+        "xtightvncviewer"},
+      
+        name = {
+          "Event Tester",
+        },
+
+        role = {
+          "AlarmWindow",
+          "ConfigManager",
+          "pop-up",
+        }
+    }, 
     
-  }
+    properties = { floating = true }
+  
+  },  
+}
